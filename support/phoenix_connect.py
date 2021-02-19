71231c43c9455066e9ee7aa69d7c2d35ad8b71e5
hickey221 <hickey.221@osu.edu>
2021-02-19 13:33:33 -0500

    added timestamps

--- support/phoenix_connect.py
+++ support/phoenix_connect.py
@@ -5,14 +5,23 @@ from bs4 import BeautifulSoup
 
 def get_update(url):
     """
-    Attempt to reach the pheonixminer webserver. Returns a requests-like object with an additional
-    text_lines attribute
+    Attempt to reach the pheonixminer webserver. Returns a dict with some useful info. If HTML code is not 200,
+    returns an error message.
     """
+    # Get HTML data
     resp = requests.get(url)
+
+    # Prepare our final returned object (dict)
+    summary = {'html_code': resp.status_code,
+               'status_text': '',
+               'full_text': '',
+               'hash rate': '',
+               'time': '',
+               'uptime': ''}
 
     # Check for HTTP code, error out if necessary
     # TODO: Handle discrete cases
-    if resp.status_code == 200:
+    if summary['html_code'] == 200:
         # Convert to lines of text
         clean_text = BeautifulSoup(resp.text, "html.parser").text
         resp.text_lines = clean_text.splitlines()
@@ -20,19 +29,29 @@ def get_update(url):
         # Remove lines that are empty or have only whitespace
         empties = ('', ' ', '\t', '\n', '&nbsp;')
         resp.text_lines = [line for line in resp.text_lines if line not in empties]
+
+        # Extract insights
+        # TODO: Replace these with some more elegant regex
+        summary['hash rate'] = resp.text_lines[-1][-11:]
+        timestamp = [line for line in resp.text_lines if line[0] == "*"][-1]
+        # Looks like ['***', '63:20', '***', '2/19', '10:16', '**************************************']
+        summary['uptime'] = timestamp.split()[1]
+        summary['time'] = timestamp.split()[4]
+
     else:
-        print(f"Warning: Got status code {resp.status_code}")
-    return resp
+        print(f"Warning: Got status code {summary['html_code']}")
+
+    return summary
 
 
 def status_line(url):
-    status = {'error': 0,
+    activity = {'error': 0,
               'text': ''}
-    resp = get_update(url)
-    if resp.status_code == 200:
-        status['text'] = f'{resp.text_lines[-1][-11:]}'
+    summary = get_update(url)
+    if summary['html_code'] == 200:
+        activity['text'] = f"{summary['hash rate']} at {summary['time']}"
     else:
-        status['error'] = 1
-        status['text'] = f"Error: Code {resp.status_code}"
+        activity['error'] = 1
+        activity['text'] = f"Error: Code {summary['html_code']}"
 
-    return status
+    return activity
