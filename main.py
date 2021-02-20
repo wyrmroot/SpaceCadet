7a4f10d3f794debdf5bddfcd1a21c55104c2883a
hickey221 <hickey.221@osu.edu>
2021-02-20 09:39:08 -0500

    Remove URL arg, added profit calculator

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ async def on_ready():
     while bot.is_ready():
         print('Fetching status')
         try:
-            current_status = phoenix_connect.status_line(MONITOR_URL)
+            current_status = phoenix_connect.status_line()
             print(f'Got: {current_status}')
             if current_status['error']:
                 await bot.change_presence(status=discord.Status.dnd,
@@ -55,6 +55,10 @@ async def on_ready():
                                                                     url=MONITOR_URL))
         except Exception as e:
             print(f'Got error {e}')
+            await bot.change_presence(status=discord.Status.dnd,
+                                      activity=discord.Activity(type=discord.ActivityType.playing,
+                                                                name="ERROR CONNECTING",
+                                                                url=MONITOR_URL))
 
         # Wait 1 minute (non-blocking)
         await asyncio.sleep(60)
@@ -64,6 +68,12 @@ async def on_ready():
 async def status(ctx):
     response = "Bot is live :tada: \n" \
                "Server is not yet connected :skull_crossbones: "
+    await ctx.send(response)
+
+
+@bot.command(name='profit', help='Get daily profit in $USD based on mining rates')
+async def profit(ctx):
+    response = phoenix_connect.get_profit()
     await ctx.send(response)
 
 
@@ -104,42 +114,6 @@ async def freq(ctx, a: int):
 async def gpu(ctx):
     response = f"Checking status of each GPU"
     await ctx.send(response)
-
-
-@bot.command(name='test', help='Test routine to change presence')
-async def test(ctx, p: int):
-    # TODO: Call the function that talks to the mining server
-    if p == 0:
-        response = "Switching to online (default)"
-        await bot.change_presence(status=discord.Status.online)
-    elif p == 1:
-        response = "Switching to connected"
-        await bot.change_presence(status=discord.Status.online,
-                                  activity=discord.Activity(type=discord.ActivityType.playing,
-                                                            name='290 MH/s, 1016W',
-                                                            url=MONITOR_URL))
-    elif p == 2:
-        response = "Switching to idle (paused)"
-        await bot.change_presence(status=discord.Status.idle,
-                                  activity=discord.Activity(type=discord.ActivityType.playing,
-                                                            name='Paused',
-                                                            url=MONITOR_URL))
-    elif p == 3:
-        response = "Switching to DND (error)"
-        await bot.change_presence(status=discord.Status.dnd,
-                                  activity=discord.Activity(type=discord.ActivityType.playing,
-                                                            name='ERROR',
-                                                            url=MONITOR_URL))
-    elif p == 4:
-        response = "Switching to custom status"
-        await bot.change_presence(status=discord.Status.online,
-                                  activity=discord.CustomActivity(name='Super awesome custom'))
-    else:
-        response = "ERR: Bad argument found"
-
-    # Wrap up and send
-    if response:
-        await ctx.send(response)
 
 
 @bot.command(name='quit', help='Fully quit the bot script. Warning: Cannot be restarted from discord')
--- support/phoenix_connect.py
+++ support/phoenix_connect.py
@@ -1,22 +1,29 @@
-import urllib.request
 import requests
+import os
+from dotenv import load_dotenv
 from bs4 import BeautifulSoup
 
 
-def get_update(url):
+load_dotenv()
+MONITOR_URL = os.getenv('SERVER_URL')
+
+
+def get_update():
     """
     Attempt to reach the pheonixminer webserver. Returns a dict with some useful info. If HTML code is not 200,
     returns an error message.
     """
     # Get HTML data
-    resp = requests.get(url)
+    resp = requests.get(MONITOR_URL)
 
     # Prepare our final returned object (dict)
+    # TODO: Convert numerics into int from str
     summary = {'html_code': resp.status_code,
                'status_text': '',
                'full_text': '',
                'hash rate': '',
                'time': '',
+               'power': '',
                'uptime': ''}
 
     # Check for HTTP code, error out if necessary
@@ -32,7 +39,8 @@ def get_update(url):
 
         # Extract insights
         # TODO: Replace these with some more elegant regex
-        summary['hash rate'] = resp.text_lines[-1][-11:]
+        summary['hash rate'] = resp.text_lines[-2].split()[-2]
+        summary['power'] = resp.text_lines[-7].split()[-2]
         timestamp = [line for line in resp.text_lines if line[0] == "*"][-1]
         # Looks like ['***', '63:20', '***', '2/19', '10:16', '**************************************']
         summary['uptime'] = timestamp.split()[1]
@@ -44,14 +52,44 @@ def get_update(url):
     return summary
 
 
-def status_line(url):
+def status_line():
     activity = {'error': 0,
               'text': ''}
-    summary = get_update(url)
+    summary = get_update()
     if summary['html_code'] == 200:
-        activity['text'] = f"{summary['hash rate']} at {summary['time']}"
+        activity['text'] = f"{summary['hash rate']} MH/s at {summary['time']}"
     else:
         activity['error'] = 1
         activity['text'] = f"Error: Code {summary['html_code']}"
 
     return activity
+
+
+def get_profit():
+    """
+    Use a website based mining calculator to figure out daily profits
+    """
+    summary = get_update()
+    hr = summary['hash rate']  # Hash rate (MH/s)
+    p = summary['power']  # Power (W)
+    fee = '1.4'  # Pool fee (%)
+    pcost = '0.16'  # Power cost ($/kWh)
+
+
+    url = f"https://whattomine.com/coins/151-eth-ethash?hr={hr}&p={p}&fee={fee}&cost={pcost}&hcost=0.0&commit=Calculate"
+    resp = requests.get(url)
+    if resp.status_code != 200:
+        print(f"Got code {resp.status_code}, aborting")
+    soup = BeautifulSoup(resp.content, "html.parser")
+    eth = soup.findAll('span')[3].text.split('\n')[2]
+    empties = ('', ' ', '\t', '\n', '&nbsp;')
+    row = [line for line in soup.find_all("tr")[2].text.split('\n') if line not in empties]
+    day_profit = row[-1]
+    response = f"Current profits: **{day_profit} / day** (ETH = {eth}).\n" \
+               f"Based on {hr}MH/s, {p}W at ${pcost}/kWh, fee {fee}%."
+    return response
+
+
+
+
+
